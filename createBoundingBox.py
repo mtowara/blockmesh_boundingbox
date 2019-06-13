75e7cba5bcc637ba89372d8b858161021ce63cbb
towara <towara@stce.rwth-aachen.de>
2019-06-13 17:31:20 +0200

    update interface of createBoundingBox.py

--- createBoundingBox.py
+++ createBoundingBox.py
@@ -10,61 +10,55 @@ def ascii_foam_vector(v):
     sv = [str(d) for d in v]
     return "( " + ' '.join(sv) + ' )'
 
+# Commandline argument parser
 parser = argparse.ArgumentParser(description='Create bounding box blockMeshDict for provided stl.')
 parser.add_argument('filename', type=str, help='filename.stl')
-parser.add_argument('--epsilon', '-e', metavar="eps", type=float, nargs='?', default=0.01, help='Additional space added to the bounding box (fraction of edge size)')
+parser.add_argument('--extra-margin', '-e', dest="eps", metavar="eps", type=float, nargs='?', default=0.01, help='Additional space added to the bounding box (fraction of edge size)')
 parser.add_argument('--spacing', '-d', metavar="d", type=float, nargs='?', default=1.0, help='Spacing')
-parser.add_argument('--direction1','-d1', metavar="f", type=float, nargs=3, default=[1.0,0.0,0.0], help='Base')
-parser.add_argument('--direction2','-d2', type=float, nargs=3, default=[0.0,1.0,0.0], help='Base')
 
 args = parser.parse_args()
 filename = args.filename
 spacing = args.spacing
-eps = args.epsilon
+eps = args.eps
 
-d1 = array(args.direction1)
-d2 = array(args.direction2)
-d1 /= norm(d1)
-d2 /= norm(d2)
-d3 = cross(d1,d2)
-
-minB = array([2**60,2**60,2**60])
-maxB = array([-2**60,-2**60,-2**60])
+block_min = array([2**60,2**60,2**60])
+block_max = array([-2**60,-2**60,-2**60])
 
 try:
     for l in open(filename,'r'):
         if "vertex" in l:
             v = [float(s) for s in l.strip('\n').split(' ')[-3:]]
             # update max
-            maxB = maximum(maxB,v)
+            block_max = maximum(block_max,v)
             # update min
-            minB = minimum(minB,v)
+            block_min = minimum(block_min,v)
 
-    delta = eps*(maxB - minB)
-    maxB += delta
-    minB -= delta
+    if (eps>0):
+        delta = eps*(block_max - block_min)
+        block_max += delta
+        block_min -= delta
 
 except IOError as e:
     print "Exception: ",e
     exit(1)
 
 vertices = ''
-vertices += "    " + ascii_foam_vector([minB[0],minB[1],minB[2]]) + '\n'
-vertices += "    " + ascii_foam_vector([maxB[0],minB[1],minB[2]]) + '\n'
-vertices += "    " + ascii_foam_vector([maxB[0],maxB[1],minB[2]]) + '\n'
-vertices += "    " + ascii_foam_vector([minB[0],maxB[1],minB[2]]) + '\n'
+vertices += "    " + ascii_foam_vector([block_min[0],block_min[1],block_min[2]]) + '\n'
+vertices += "    " + ascii_foam_vector([block_max[0],block_min[1],block_min[2]]) + '\n'
+vertices += "    " + ascii_foam_vector([block_max[0],block_max[1],block_min[2]]) + '\n'
+vertices += "    " + ascii_foam_vector([block_min[0],block_max[1],block_min[2]]) + '\n'
 
-vertices += "    " + ascii_foam_vector([minB[0],minB[1],maxB[2]]) + '\n'
-vertices += "    " + ascii_foam_vector([maxB[0],minB[1],maxB[2]]) + '\n'
-vertices += "    " + ascii_foam_vector([maxB[0],maxB[1],maxB[2]]) + '\n'
-vertices += "    " + ascii_foam_vector([minB[0],maxB[1],maxB[2]]) + '\n'
+vertices += "    " + ascii_foam_vector([block_min[0],block_min[1],block_max[2]]) + '\n'
+vertices += "    " + ascii_foam_vector([block_max[0],block_min[1],block_max[2]]) + '\n'
+vertices += "    " + ascii_foam_vector([block_max[0],block_max[1],block_max[2]]) + '\n'
+vertices += "    " + ascii_foam_vector([block_min[0],block_max[1],block_max[2]]) + '\n'
 
-grading = ascii_foam_vector([int(ceil((maxB[0]-minB[0])/spacing)), \
-                             int(ceil((maxB[1]-minB[1])/spacing)), \
-                             int(ceil((maxB[2]-minB[2])/spacing))])
+n_cells = ascii_foam_vector([int(ceil((block_max[0]-block_min[0])/spacing)), \
+                             int(ceil((block_max[1]-block_min[1])/spacing)), \
+                             int(ceil((block_max[2]-block_min[2])/spacing))])
 try:
-    template = open('bb_template','r').read()
+    template = open('blockMeshDict.template','r').read()
     print template.replace('$vertices',vertices.strip('\n')) \
-                  .replace('$grading',grading)
+                  .replace('$nCells',n_cells)
 except IOError as e:
     print "Exception: ",e
