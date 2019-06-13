4efce951122ff4cbf9a147e681ee3c8dd267a9a5
towara <towara@stce.rwth-aachen.de>
2019-06-13 17:34:09 +0200

    create paraview foam files

--- test.sh
+++ test.sh
@@ -2,10 +2,12 @@
 
 # create othogonal bounding box 
 cp -r testCaseStub testCaseOrth
+touch testCaseOrth/foam.foam
 ./createBoundingBox.py --extra-margin 0.05 --spacing 2.0 test.stl > testCaseOrth/system/blockMeshDict
 blockMesh -case testCaseOrth
 
 # create bounding box aligned to object
 cp -r testCaseStub testCaseCustomAxes
+touch testCaseCustomAxes/foam.foam
 ./createBoundingBoxCustomAxes.py --extra-margin 0.05 --spacing 2.0 -d1 49.497 35.000 -35.00 -d2 0.000 7.071 7.071 test.stl > testCaseCustomAxes/system/blockMeshDict
 blockMesh -case testCaseCustomAxes
